4beb5a1d080a79cc01f53239158c6c468c8c808e
bityta <bituckihs@mail.com>
2023-10-17 03:03:15 +0300

    добавление документации java doc

--- src/main/java/org/example/utils/ConnectionManager.java
+++ src/main/java/org/example/utils/ConnectionManager.java
@@ -4,19 +4,41 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * Класс, который обеспечивает подключение к БД
+ */
 public final class ConnectionManager {
 
+    /**
+     * Пароль-ключ по которому храниться значение в конфиг файле
+     */
     private static final String PASSWORD_KEY = "db.password";
+
+    /**
+     * Название-ключ по которому храниться значение в конфиг файле
+     */
     private static final String USERNAME_KEY = "db.username";
+
+    /**
+     * URL-ключ по которому храниться значение в конфиг файле
+     */
     private static final String URL_KEY = "db.url";
 
     static {
         loadDriver();
     }
 
+    /**
+     * Приватный конструктор
+     */
     private ConnectionManager() {
     }
 
+    /**
+     * Выполнение подлючения к БД
+     *
+     * @return connection
+     */
     public static Connection open() {
         try {
             return DriverManager.getConnection(
@@ -29,6 +51,9 @@ public final class ConnectionManager {
         }
     }
 
+    /**
+     * Подгрузка драйвера PostgreSQL
+     */
     private static void loadDriver() {
         try {
             Class.forName("org.postgresql.Driver");
